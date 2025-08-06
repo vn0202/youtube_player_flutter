8ce6bbd1aecea8755155d7731464beb812b1f26c
vn0202 <94747520+vn0202@users.noreply.github.com>
2025-08-06 15:14:34 +0700

    Update raw_youtube_player.dart

--- packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
+++ packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
@@ -73,7 +73,6 @@ class _RawYoutubePlayerState extends State<RawYoutubePlayer>
         key: widget.key,
         initialData: InAppWebViewInitialData(
           data: player,
-          baseUrl: WebUri.uri(Uri.https('www.youtube.com')),
           encoding: 'utf-8',
           mimeType: 'text/html',
         ),
